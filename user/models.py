a22df74dcd1652f051782b4219d2356d2124bc28
vlad <vladvoytkiv321@gmail.com>
2025-04-15 14:35:52 +0300

    Remove unused logic

--- user/models.py
+++ user/models.py
@@ -1,9 +1,5 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from cloudinary.models import CloudinaryField
-import os
-
-DEFAULT_AVATAR_URL = os.getenv('DEFAULT_AVATAR_URL')
 
 
 class User(AbstractUser):
@@ -11,7 +7,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
-    avatar = CloudinaryField('image', blank=True, null=True)
+
 
     groups = models.ManyToManyField(
         Group,
@@ -25,15 +21,3 @@ class User(AbstractUser):
         related_name='custom_user_set',
         related_query_name='custom_user',
     )
-
-    def save(self, *args, **kwargs):
-        if not self.avatar or self.avatar =="":
-            self.avatar = DEFAULT_AVATAR_URL
-        super().save(*args, **kwargs)
-
-    @property
-    def avatar_url(self):
-        if self.avatar:
-            return self.avatar.url
-        else:
-            return DEFAULT_AVATAR_URL
