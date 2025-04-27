32dd893fd59340955db80250e55a5f0fc761a48b
vlad <vladvoytkiv321@gmail.com>
2025-04-27 22:52:48 +0300

    Implement business Logic for Posts

---
+++ profiles/services/profile_service.py
@@ -0,0 +1,39 @@
+from rest_framework import status
+
+from profiles.serializer import CreateProfileSerializer, ProfileSerializer
+from profiles.models import Profile
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+class ProfileService:
+    @staticmethod
+    def get_profile(request):
+        try:
+            profile = Profile.objects.get(user=request.user)
+            serializer = ProfileSerializer(profile, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            raise NotFound(detail='Profile not found', code='404')
+
+    @staticmethod
+    def create_profile(data, user):
+        serializer = CreateProfileSerializer(data=data, context={'user': user})
+        serializer.is_valid(raise_exception=True)
+        profile = serializer.save()
+        return ProfileSerializer(profile)
+
+    @staticmethod
+    def update_profile(data, user, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(data=request.data, instance=profile, partial=True)
+        serializer.is_valid(raise_exception=True)
+        profile = serializer.save()
+        return ProfileSerializer(profile)
+
+    @staticmethod
+    def delete_profile(data, user, request):
+        try:
+            profile = Profile.objects.get(user=request.user)
+            profile.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Profile.DoesNotExist:
+            raise NotFound(detail='Profile not found', code='404')
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles.services.profile_service import ProfileService
+
+class GetProfile(APIView):
+    @staticmethod
+    def get(request, slug = None):
+        serializer = ProfileService.get_profile(request=request)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CreateProfile(APIView):
+    @staticmethod
+    def post(request):
+        serializer = ProfileService.create_profile(user=request.user, data=request.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class EditProfile(APIView):
+    @staticmethod
+    def patch(request, slug = None, ):
+        serializer = ProfileService.update_profile(data=request.data, user=request.user, request=request)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DeleteProfile(APIView):
+    @staticmethod
+    def delete(request, slug = None):
+        serializer = ProfileService.delete_profile(data=request.data, user=request.user, request=request)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
