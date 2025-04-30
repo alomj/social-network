50713dbc1388f4850226b87a9f229301e3688bd5
vlad <vladvoytkiv321@gmail.com>
2025-04-30 19:20:19 +0300

    Refactoring stuff

--- profiles/services/profile_service.py
+++ profiles/services/profile_service.py
@@ -5,9 +5,10 @@ from rest_framework.exceptions import NotFound
 
 class ProfileGetter:
     @staticmethod
-    def get_profile(request):
+    def get_profile(slug):
         try:
-            return Profile.objects.get_profile_by_user_request(request.user)
+            profile = Profile.objects.get_profile_by_user_slug(slug)
+            return profile
         except Profile.DoesNotExist:
             raise NotFound(detail='Profile not found', code='404')
 
@@ -23,9 +24,8 @@ class ProfileCreator:
 
 class ProfileUpdater:
     @staticmethod
-    def update_profile(request):
-        profile = Profile.objects.get_profile_by_user_request(request.user)
-        serializer = ProfileSerializer(data=request.data, instance=profile, partial=True)
+    def update_profile(profile, data):
+        serializer = ProfileSerializer(data=data, instance=profile, partial=True)
         serializer.is_valid(raise_exception=True)
         profile = serializer.save()
         return ProfileSerializer(profile)
@@ -35,7 +35,7 @@ class ProfileDeleter:
     @staticmethod
     def delete_profile(request):
         try:
-            profile = Profile.objects.get_profile_by_user_request(request.user)
+            profile = Profile.objects.get_profile_by_user_slug(request.user)
             profile.delete()
         except Profile.DoesNotExist:
             raise NotFound(detail='Profile not found', code='404')
