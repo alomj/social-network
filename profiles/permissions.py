cd89db77e144b7757015b4c8db44cabe32e2f688
vlad <vladvoytkiv321@gmail.com>
2025-04-30 20:25:24 +0300

    Implement permissions check for API

---
+++ profiles/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+class ReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,32 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+
+from profiles.permissions import IsOwner
+from profiles.serializer import ProfileSerializer
 from profiles.services.profile_service import ProfileDeleter, ProfileGetter, ProfileCreator, ProfileUpdater
+from django.core.cache import cache
 
 
 class GetProfile(APIView):
-    @staticmethod
-    def get(request, slug=None):
-        serializer = ProfileGetter.get_profile(request=request)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, slug):
+        cache_key = f'profile-cache-{slug}'
+        data = cache.get(cache_key)
+        if not data:
+            profile = ProfileGetter.get_profile(slug)
+            serializer = ProfileSerializer(profile)
+            data = serializer.data
+            cache.set(cache_key, data, timeout=60 * 10)
+        return Response(data, status=status.HTTP_200_OK)
 
 
 class CreateProfile(APIView):
     permission_classes = [IsAuthenticated]
+
     @staticmethod
     def post(request):
         serializer = ProfileCreator.create_profile(user=request.user, data=request.data)
@@ -21,9 +34,12 @@ class CreateProfile(APIView):
 
 
 class EditProfile(APIView):
-    @staticmethod
-    def patch(request, slug=None):
-        serializer = ProfileUpdater.update_profile(request=request)
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def patch(self, request, slug):
+        profile = ProfileGetter.get_profile(slug)
+        self.check_object_permissions(request, profile)
+        serializer = ProfileUpdater.update_profile(profile, request.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
