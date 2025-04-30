2a1c4e1234cdc78fa39af47d924920c3586a3b5f
vlad <vladvoytkiv321@gmail.com>
2025-04-30 19:19:48 +0300

    add signals

---
+++ profiles/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+
+from profiles.models import Profile
+from user.models import User
+
+@receiver([post_save, post_delete], sender=Profile)
+def remove_cache(sender, instance, **kwargs):
+    cache_key = f'profile-cache-{instance.slug}'
+    cache.delete(cache_key)
+
+
+@receiver([post_save], sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver([post_save], sender=User)
+def update_user_info(sender, instance, **kwargs):
+    instance.profile.save()
